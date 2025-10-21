16a40d1d5746123f8c1a661a79d992a387514fe1
dream30915 <dream30915@gmail.com>
2025-10-22 04:40:01 +0700

    Fix: Register NextPlotService and SupabaseService in Service Container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Services\SupabaseSqlClient;
+use App\Services\SupabaseService;
+use App\Services\NextPlotService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // Register SupabaseSqlClient
         $this->app->singleton(SupabaseSqlClient::class, function ($app) {
             $config = $app['config']->get('services.supabase', []);
 
@@ -19,6 +22,16 @@ class AppServiceProvider extends ServiceProvider
                 $config['url'] ?? '',
                 $config['service_role_key'] ?? '',
             );
+        });
+
+        // Register SupabaseService (required by NextPlotService)
+        $this->app->singleton(SupabaseService::class, function ($app) {
+            return new SupabaseService();
+        });
+
+        // Register NextPlotService (depends on SupabaseService)
+        $this->app->singleton(NextPlotService::class, function ($app) {
+            return new NextPlotService($app->make(SupabaseService::class));
         });
     }
 
